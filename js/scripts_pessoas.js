a6649294dc2e1e3af31778f8b9f3a3b9fb1c65ad
vivian <vivian.g.souza6@aluno.senai.br>
2026-06-22 15:32:51 -0300

    arquivo comitado

--- js/scripts_pessoas.js
+++ js/scripts_pessoas.js
@@ -11,7 +11,7 @@ formPessoa.addEventListener('submit', (evt) => {
     evt.preventDefault ()
 
     //CRIAR UM OBJETO FORMULÁRIO
-    const dadosFormPessoa = new FormData(formPessoa)
+    const dadosFormPessoa = new FormData (formPessoa)
 
     //CRIAR UM ONJETO LITERAL 
     const pessoa = {
@@ -20,12 +20,25 @@ formPessoa.addEventListener('submit', (evt) => {
         renda: dadosFormPessoa.get('renda')
     }
 
-      //CHAMANDO FUNÇÃO addPessoa E PASSANDO O OBJETO LITERAL pessoa
-      addPessoa(pessoa)
+    //CHAMANDO FUNÇÃO addPessoa E PASSANDO O OBJETO LITERAL pessoa
+    addPessoa(pessoa)
 
 })
 
 //CRIANDO A FUNÇÃO ADICIONAR PESSOA
 const addPessoa = (objPessoa) => {
+    //ADICIONANDO O OBJETO LITERAL NO ARRAY PESSOAS
     pessoas.push(objPessoa)
+
+    //APÓS ADICIONAR O OBJETO LITERAL NO ARRAY CHAMA A FUNÇÃO listPessoas
+    listPessoas ()
+}
+
+//FUNÇÂO PARA LISTAR PESSOAS DO ARRAY
+const listPessoas = () => {
+    //PERCORRER O ARRAY pessoas COM O forEach
+    pessoas.forEach((elem, i) => {
+        divLista.innerHTML += `${i} - ${elem.nome} - ${elem.idade} - ${elem.renda} <br>`
+    })
+
 }
